3109ab98c361b3ec29f4419fdd640f4a7949b5a8
Alexandre Negrel <negrel.dev@protonmail.com>
2021-02-20 22:19:52 +0100

    adding magie cli tool crates

---
+++ cmd/src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::fs::File;
+
+use libmagei::codec::bitmap;
+
+fn main() {
+    println!("{}", env::current_dir().unwrap().display());
+
+    let mut file = match File::open("../images/rgb.bmp") {
+        Err(e) => panic!(e),
+        Ok(file) => file,
+    };
+
+    let _ = bitmap::decoder::decode(&mut file);
+}
