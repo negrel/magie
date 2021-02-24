e993c7f08e119c04f6fdfffe459c7ebcb9783657
Alexandre Negrel <negrel.dev@protonmail.com>
2021-02-24 23:15:52 +0100

    decoder takes a Reader as input now

--- src/codec/bitmap/decoder.rs
+++ src/codec/bitmap/decoder.rs
@@ -1,11 +1,10 @@
-use std::fs::File;
 use std::io::Read;
 
 use crate::codec::bitmap::*;
 
 pub struct Decoder {}
 
-pub fn decode(img: &mut File) -> Result<ImageBitmap, Error> {
+pub fn decode(img: &mut dyn Read) -> Result<ImageBitmap, Error> {
     let mut buf_header: [u8; HEADER_SIZE] = [0; HEADER_SIZE];
     let _ = img.read_exact(&mut buf_header);
 
